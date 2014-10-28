b0c7a54d5112e9260405e9f79822814510469e5f
Amandeep Singh <amandeep.bhamra@gmail.com>
2014-10-29 01:55:54 +0530

    raw

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MessMail::Application.config.secret_token = '276ba74b4ad64dd0ad4c896f39687278c8c63704fcd2eaec3d52ace68807dd90268bb72c95414b5f2b696d1fca8cd26e92d9a34a79b6a62fd75e14af6ca23ee0'
